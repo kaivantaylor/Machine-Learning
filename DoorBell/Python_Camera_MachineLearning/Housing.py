e6d44c8851a58bc5eda36d439f90c84a5d9cb86c
Kaivan Taylor <38149120+speedykai@users.noreply.github.com>
2018-12-21 08:35:55 -0500

    Housing

---
+++ DoorBell/Python_Camera_MachineLearning/Housing.py
@@ -0,0 +1,32 @@
+# PURPOSE: Create a script that fetches from a URL and automatically updates
+
+# imports
+import os
+import tarfile
+import pandas as pd
+import matplotlib.pyplot as plt
+from six.moves import urllib
+
+# main
+
+# variable declarations
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+# fetch_housing_data(housing_url, housing_path) - Fetches data from URL, creates directory
+
+def fetch_housing_data(housing_url = HOUSING_URL, housing_path = HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+# load_housing_data(housing_path) - Loads data from URL and formats in a table
+
+def load_housing_data(housing_path = HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
